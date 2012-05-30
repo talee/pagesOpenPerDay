45927c0b644072d24ee11e404bcd2a794bfcbb69
T Lee <tleehoc@gmail.com>
2012-05-30 05:18:02 -0700

    Optimized awk; Fix environment to allow cron usage

--- getHistory.sh
+++ getHistory.sh
@@ -3,19 +3,24 @@ DIR="histories/"
 STOREDIR="records/"
 DATE=$(date +'%b %d, %Y')
 OUTPUTFILE=$DIR$DATE
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+unset POSIXLY_CORRECT
+#. ~/.bashrc
+. /etc/profile
+. ~/.profile
 cd "$( cd "$( dirname "$0" )" && pwd )"
-cp ~/.opera-next/global_history.dat .
-awk -f getTodayHistory.awk -- global_history.dat > "$OUTPUTFILE"
+tac ~/.opera-next/global_history.dat > global_history.dat
+/usr/bin/gawk -f getTodayHistory.awk -- global_history.dat > "$OUTPUTFILE"
 rm global_history.dat
-sed -i -f removeExtraHistory.sed "$OUTPUTFILE"
+/bin/sed -i -f removeExtraHistory.sed "$OUTPUTFILE"
 echo -n "From "
 # grep -Z doesn't remove newline for some reason or other... use tr then
-grep -o -m 1 '[0-9]\{2\}:[0-9]\{2\}[AP]M' "$OUTPUTFILE" | tr -d '\n'
+tac "$OUTPUTFILE" | grep -o -m 1 '[0-9]\{2\}:[0-9]\{2\}[AP]M' | tr -d '\n'
 echo -n " to "
-tac "$OUTPUTFILE" | grep -o -m 1 '[0-9]\{2\}:[0-9]\{2\}[AP]M'
+/bin/grep -o -m 1 '[0-9]\{2\}:[0-9]\{2\}[AP]M' "$OUTPUTFILE"
 echo -n "I opened about "
 NUMLINES=$(cat "$OUTPUTFILE" | wc -l | tr -d '\n')
 echo -n $NUMLINES
 echo " web pages in the Opera browser."
-find records/ -type f -print0 -name '$DATE*' | xargs -0 rm
+find records/ -name "$DATE*" -print0 | xargs -0 rm
 echo > $STOREDIR$DATE" - "$NUMLINES
--- getTodayHistory.awk
+++ getTodayHistory.awk
@@ -1,23 +1,26 @@
 # /usr/bin/awk -f
-# Preserve Opera history entries with today's date
+# Preserve Opera history entries with today's date. Assumes reversed file.
 
 BEGIN {
     # Get today's day of the month
     today = strftime("%b %d, %Y",systime());
 }
 {
-    title     = $0;
-    getline;
-    URL       = $0;
+    priority  = $0;
     getline;
     date      = $0;
     getline;
-    priority  = $0;
+    URL       = $0;
+    getline;
+    title     = $0;
 
     # Replace Unix Epoch time with month day, year
     day = strftime("%b %d, %Y", date);
     if ( day == today ) {
         sub(".*$", day);
         print strftime("%I:%M%p", date), title, URL;
+    }
+    else if ( day < today ) {
+        exit;
     }
 }
